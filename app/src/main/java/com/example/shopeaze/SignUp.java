641b6c3aecc7a8a70a0fcdcc5368b8c69faec109
Elizabeth <eilina4162@gmail.com>
2023-07-31 13:15:29 -0400

    Updated Shopper.java and SignUp.java

--- app/src/main/java/com/example/shopeaze/SignUp.java
+++ app/src/main/java/com/example/shopeaze/SignUp.java
@@ -22,6 +22,12 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUp extends Fragment {
 
@@ -31,17 +37,19 @@ public class SignUp extends Fragment {
     ProgressBar progressBar;
     TextView textView;
 
+    FirebaseDatabase db;
+
 
     @Override
     public void onStart() {
         super.onStart();
+        //new line:
+        db = FirebaseDatabase.getInstance("https://grocery-d4fbb-default-rtdb.firebaseio.com//");
+        //old:
         mAuth = FirebaseAuth.getInstance();
         // Check if user is already signed in (non-null)
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
-            //Intent intent = new Intent(getActivity(), MainActivity.class);
-            //startActivity(intent);
-            //getActivity().finish();
             NavHostFragment.findNavController(SignUp.this)
                     .navigate(R.id.action_logout_to_WelcomeScreen);
         }
@@ -66,9 +74,6 @@ public class SignUp extends Fragment {
         textView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //Intent intent = new Intent(getActivity(), Login.class);
-                //startActivity(intent);
-                //getActivity().finish();
                 NavHostFragment.findNavController(SignUp.this)
                         .navigate(R.id.action_signUp_to_Login);
             }
@@ -77,6 +82,7 @@ public class SignUp extends Fragment {
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                DatabaseReference ref= db.getReference();   //new
                 progressBar.setVisibility(View.VISIBLE);
                 String email;
                 String password;
@@ -106,11 +112,9 @@ public class SignUp extends Fragment {
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
+                                    //ref.child("Users").child("Shopper").child("Email").setValue(email); //new line
                                     Toast.makeText(getActivity(), "Account Created.",
                                             Toast.LENGTH_SHORT).show();
-                                    //Intent intent = new Intent(getActivity(), Login.class);
-                                    //startActivity(intent);
-                                    //getActivity().finish();
                                     NavHostFragment.findNavController(SignUp.this)
                                             .navigate(R.id.action_signUp_to_Login);
                                 } else {
