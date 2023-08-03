7fa0dab1e19fcd98e3789521f099ba57afcd7ad9
Katherine Pravdin <katherinepravdin@gmail.com>
2023-08-03 00:12:00 -0400

    Fixed buttons

---
+++ app/src/main/java/com/example/shopeaze/PageInterface.java
@@ -0,0 +1,9 @@
+package com.example.shopeaze;
+
+import android.view.View;
+
+interface PageInterface {
+//    abstract View ordersButtonOnClick(View v);
+//    abstract View ordersButtonOnClick(View v);
+
+}
