beac2cbd7715b0426bc47ef542d2cceef9f919ff
Katherine Pravdin <katherinepravdin@gmail.com>
2023-08-03 20:02:38 -0400

    Removing unneeded Interface

--- app/src/main/java/com/example/shopeaze/PageInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopeaze;
-
-import android.view.View;
-
-interface PageInterface {
-//    abstract View ordersButtonOnClick(View v);
-//    abstract View ordersButtonOnClick(View v);
-
-}
