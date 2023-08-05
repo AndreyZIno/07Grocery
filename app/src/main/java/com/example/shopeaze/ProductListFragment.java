cf99c39cd9f3948f98b5df2ca6ad608ce30b15bd
the creator <chenxi.yan@gmail.com>
2023-08-05 00:18:10 -0400

    Removing unnecessary imports

--- app/src/main/java/com/example/shopeaze/ProductListFragment.java
+++ app/src/main/java/com/example/shopeaze/ProductListFragment.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.FloatingWindow;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
