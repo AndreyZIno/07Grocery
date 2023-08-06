201c2ccd0f548559303df5a407c06951ab7b8374
Andrey Zinovyev <andrey.znvyev@gmail.com>
2023-08-05 20:16:13 -0400

    msg

--- app/src/main/java/com/example/shopeaze/OrderFragment.java
+++ app/src/main/java/com/example/shopeaze/OrderFragment.java
@@ -1,16 +1,34 @@
 package com.example.shopeaze;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
@@ -23,65 +41,135 @@ public class OrderFragment extends Fragment {
     private FirebaseFirestore db;
 
     // Declare a ListView to display the orders and a Button for refreshing the orders.
-    private ListView ordersListView;
     private Button refreshButton;
 
-    // Declare an ArrayAdapter to handle the list of orders.
-    private ArrayAdapter<String> ordersAdapter;
-
     // Declare a List to hold the orders data.
-    private List<String> ordersList;
+    private RecyclerView recyclerView;
+    private OrdersAdapter adapter;
+    private List<Order> orderList = new ArrayList<>();
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_orders, container, false);
 
-        ordersListView = view.findViewById(R.id.ordersListView);
+        recyclerView = view.findViewById(R.id.recyclerViewOrders);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Initialize the refresh Button and set a click listener.
+        adapter = new OrdersAdapter(orderList);
+        recyclerView.setAdapter(adapter);
+
         Button refreshButton = view.findViewById(R.id.refreshButton);
-        refreshButton.setOnClickListener(v -> loadOrders());
+        refreshButton.setOnClickListener(v -> {
+            fetchData();
+        });
 
-        mAuth = FirebaseAuth.getInstance();
-        db = FirebaseFirestore.getInstance();
+        ImageButton ordersButton = view.findViewById(R.id.button_orders);
+        ordersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = NavHostFragment.findNavController(OrderFragment.this);
+                NavDestination currentDestination = navController.getCurrentDestination();
+                if (currentDestination != null && currentDestination.getId() == R.id.OrderFragment) {
+                    // User is already on OrderFragment, do nothing
+                    return;
+                }
+            }
+        });
 
-        loadOrders();
+        fetchData();
+
+        ImageButton storesButton = view.findViewById(R.id.button_stores);
+        storesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = NavHostFragment.findNavController(OrderFragment.this);
+                navController.navigate(R.id.action_OrderFragment_to_StoreList);
+            }
+        });
+
+        ImageButton cartButton = view.findViewById(R.id.button_cart);
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavController navController = NavHostFragment.findNavController(OrderFragment.this);
+                navController.navigate(R.id.action_OrderFragment_to_Cart);
+            }
+        });
 
         return view;
     }
 
 
     // Function to load orders from Firestore.
-    private void loadOrders() {
-        // Get the current user's ID.
-        String userId = mAuth.getCurrentUser().getUid();
+    private void fetchData() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("orders");
+        Query query = ref.orderByChild("userId").equalTo(FirebaseAuth.getInstance().getCurrentUser().getUid());
 
-        // Query Firestore for orders where the userId matches the current user's ID.
-        db.collection("orders")
-                .whereEqualTo("userId", userId)
-                .get()
-                .addOnCompleteListener(task -> {
-                    // If the query is successful...
-                    if (task.isSuccessful()) {
-                        // Initialize the ordersList.
-                        ordersList = new ArrayList<>();
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                orderList.clear();
+                for (DataSnapshot orderSnapshot : dataSnapshot.getChildren()) {
+                    Order order = orderSnapshot.getValue(Order.class);
+                    orderList.add(order);
+                }
+                adapter.notifyDataSetChanged();
+            }
 
-                        // Loop through each document in the results.
-                        for (QueryDocumentSnapshot document : task.getResult()) {
-                            // Convert the document into an Order object.
-                            Order order = document.toObject(Order.class);
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("OrdersFragment", "onCancelled", databaseError.toException());
+            }
+        });
+    }
 
-                            // Add the order's ID and status to the ordersList.
-                            ordersList.add("Order ID: " + order.getOrderId() + "\nStatus: " + order.getStatus());
-                        }
-
-                        // Create an ArrayAdapter with the ordersList and set it as the adapter for ordersListView.
-                        ordersAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, ordersList);
-                        ordersListView.setAdapter(ordersAdapter);
-                    } else {
-                        // If the query is not successful, display a toast with an error message.
-                        Toast.makeText(getContext(), "Error getting orders", Toast.LENGTH_SHORT).show();
-                    }
-                });
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final NavController navController = NavHostFragment.findNavController(this);
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                ImageButton storesButton = view.findViewById(R.id.button_stores);
+                ImageButton cartButton = view.findViewById(R.id.button_cart);
+                ImageButton ordersButton = view.findViewById(R.id.button_orders);
+                TextView storesText = view.findViewById(R.id.textViewStoresText);
+                TextView cartText = view.findViewById(R.id.textViewCartText);
+                TextView ordersText = view.findViewById(R.id.textViewOrdersText);
+                ImageView storeIcon = view.findViewById(R.id.storeIcon);
+                ImageView cartIcon = view.findViewById(R.id.cartIcon);
+                ImageView ordersIcon = view.findViewById(R.id.ordersIcon);
+                if (destination.getId() == R.id.StoreList) {
+                    storesButton.setImageResource(R.drawable.focused_nav_button);
+                    cartButton.setImageResource(R.drawable.nav_gradient);
+                    ordersButton.setImageResource(R.drawable.nav_gradient);
+                    storesText.setTextColor(ContextCompat.getColor(getContext(), R.color.navy_blue));
+                    cartText.setTextColor(ContextCompat.getColor(getContext(), R.color.light_gray));
+                    ordersText.setTextColor(ContextCompat.getColor(getContext(), R.color.light_gray));
+                    storeIcon.setImageResource(R.drawable.black_store);
+                    cartIcon.setImageResource(R.drawable.white_cart);
+                    ordersIcon.setImageResource(R.drawable.white_orders);
+                } else if (destination.getId() == R.id.Cart) {
+                    storesButton.setImageResource(R.drawable.nav_gradient);
+                    cartButton.setImageResource(R.drawable.focused_nav_button);
+                    ordersButton.setImageResource(R.drawable.nav_gradient);
+                    storesText.setTextColor(ContextCompat.getColor(getContext(), R.color.light_gray));
+                    cartText.setTextColor(ContextCompat.getColor(getContext(), R.color.navy_blue));
+                    ordersText.setTextColor(ContextCompat.getColor(getContext(), R.color.light_gray));
+                    storeIcon.setImageResource(R.drawable.white_store);
+                    cartIcon.setImageResource(R.drawable.black_cart);
+                    ordersIcon.setImageResource(R.drawable.white_orders);
+                } else if (destination.getId() == R.id.OrderFragment) {
+                    storesButton.setImageResource(R.drawable.nav_gradient);
+                    cartButton.setImageResource(R.drawable.nav_gradient);
+                    ordersButton.setImageResource(R.drawable.focused_nav_button);
+                    storesText.setTextColor(ContextCompat.getColor(getContext(), R.color.light_gray));
+                    cartText.setTextColor(ContextCompat.getColor(getContext(), R.color.light_gray));
+                    ordersText.setTextColor(ContextCompat.getColor(getContext(), R.color.navy_blue));
+                    storeIcon.setImageResource(R.drawable.white_store);
+                    cartIcon.setImageResource(R.drawable.white_cart);
+                    ordersIcon.setImageResource(R.drawable.black_orders);
+                }
+            }
+        });
     }
 }
